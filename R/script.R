1e7da8140700cfb70ca71fe6a2540317a9c1a037
Raphael <raphaelv@arcor.de>
2021-10-12 12:31:42 +0200

    tried my best with import but doesnt work - help Simone!

--- R/script.R
+++ R/script.R
@@ -11,23 +11,11 @@ options(warn=-1)# warnung ausblenden
 #Input von andere Team _Condition_code=E84.0,E84.1,E84.80,E84.87,E84.88,E84.9,O80_2021-03-03_15-25-58
 #data <- read.csv("r/projectathon/filename.csv")
 ###############################################################################################################
-data_folder <- "/opt/pht_data/"
-result_folder <- "/opt/pht_results/"
+data_folder <- "./opt/pht_data/"
+result_folder <- "./opt/pht_results/"
 print(paste(data_folder, "A2-1.csv", sep = ""))
 
 data <- read.csv(paste(data_folder ,"A2-1.csv", sep = ""))# aus projektbereich ordner
-
-#wenn im PHT zuvor daten erstellt wurde -> auslesen
-if (file.exists(paste(result_folder,"input.csv", sep = ""))) {
-  
-  data_pht <- read.csv(paste(result_folder,"input.csv", sep = ""))#("pht/results.csv")
-  
-  data <- rbind(data, data_pht)
-  message("previous PHT result found -> Add up")
-} else {
-  
-  message("No previous PHT result found -> Assume first")
-}
 
 
 # Eleminiere doppelte Patienten
@@ -37,6 +25,31 @@ data$PatientIdentifikator <- NULL
 # Berechne Alter auf der grund von Geburtsdatum
 data$AngabeAlter <- floor(age_calc(as.Date(data$AngabeGeburtsdatum), unit="years"))
 data$AngabeGeburtsdatum <- NULL
+
+
+#WRITE Mean Data for PHT and add up if available ---------------------------
+data_pht_man = data %>% subset(AngabeGeschlecht=="m")
+data_pht_woman = data %>% subset(AngabeGeschlecht=="f")
+output_pht = c(length(data_pht_man$AngabeAlter), as.double(mean(data_pht_man$AngabeAlter)), length(data_pht_woman$AngabeAlter), as.double(mean(data_pht_woman$AngabeAlter)))
+
+#Check if there are previous results -> if yes add up
+if (file.exists(paste(result_folder,"result_mean.csv", sep = ""))) {
+  
+  previous_mean <- read.table(paste(result_folder,"result_mean.csv", sep = ""), 
+                              header = FALSE)
+  i<-0
+  for (i in 1:4){ 
+    output_pht[i] <- output_pht[i] + as.double(previous_mean$V1[i+1])
+    print(previous_mean$V1[i+1])
+  }
+  print(output_pht)
+  #data <- rbind(stratified_wide, data_pht)
+  message("previous PHT result found -> Add up")
+}
+print(output_pht)
+write.csv(output_pht, "./opt/pht_results/result_mean.csv", row.names = FALSE)
+#----------------------------------------------------------------------------
+
 
 # Teile in Altersgruppen ein
 data$AngabeAlter <- cut(data$AngabeAlter, breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120))
@@ -52,14 +65,6 @@ result$TextDiagnose2 <- NULL
 result$AngabeDiag2 <- NULL
 
 
-#WRITE SOME Data for PHT
-data_pht_man = data %>% subset(AngabeGeschlecht=="m")
-data_pht_woman = data %>% subset(AngabeGeschlecht=="f")
-output_pht = c(length(data_pht_man$AngabeAlter), mean(data_pht_man$AngabeAlter), length(data_pht_woman$AngabeAlter), mean(data_pht_woman$AngabeAlter))
-print(output_pht)
-write.csv2(output_pht, "./opt/pht_results/result_mean.csv", row.names = FALSE)
-
-
 ################## Um der Alterspyramid zu rechnen######################################################################
 # Nehmen wir Geschlechht, Alter, Anzahl
 ############################################################################################################################
@@ -72,8 +77,21 @@ stratified_wide <- rbind(stratified_female,stratified_male)
 stratified_wide$AngabeGeschlecht [stratified_wide$AngabeGeschlecht == "f"] <- "female"
 stratified_wide$AngabeGeschlecht [stratified_wide$AngabeGeschlecht == "m"] <- "male"
 
-#F�R PHT DAS GANZE RAUSSCHREIBEN
-write.csv(stratified_wide, "/opt/pht_results/result_table.csv")
+
+#wenn im PHT zuvor daten erstellt wurde -> auslesen
+if (file.exists(paste(result_folder,"result_table.csv", sep = ""))) {
+  
+  data_pht <- read.csv(paste(result_folder,"result_table.csv", sep = ""))#("pht/results.csv")
+  
+  data <- rbind(stratified_wide, data_pht)
+  message("previous PHT result found -> Add up")
+} else {
+  
+  message("No previous PHT result found -> Assume first")
+}
+
+#FUER PHT DAS GANZE RAUSSCHREIBEN - inklusive neuen Daten
+write.csv(stratified_wide, "./opt/pht_results/result_table.csv")
 
 #Labellen name als angabe
 names(stratified_wide)[names(stratified_wide)== "AngabeAlter"] <- "ageG"
